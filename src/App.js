5646741de7803620651e744ecb57f721497171f1
Elvira Ramírez <elvirarp92@gmail.com>
2020-12-02 18:25:24 +0100

    App now takes state from /salons API route
    This will be subject to many refactors, to deal with authentication and
    multiple requests to the API in one page. So far this is a step in that
    direction for component testing purposes.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import { Button } from './components/atoms'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 function App() {
+  const cancelTokenSource = axios.CancelToken.source()
+
+  const [salons, setSalons] = useState([])
+  const [err, setError] = useState(undefined)
+
+  function refreshSalons() {
+    axios
+      .get(`http://localhost:5000/salons`, { cancelToken: cancelTokenSource.token })
+      .then((response) => setSalons(response.data))
+      .catch((err) => {
+        if (axios.isCancel(err)) {
+          console.log('call cancelled')
+        } else {
+          setError(err)
+        }
+      })
+  }
+
+  useEffect(() => refreshSalons(), [])
+
   return (
-    <div style={{ display: 'flex' }}>
-      <Button isSolid color='red'>Default</Button>
-      <Button isSolid color='blue'>Blue BG</Button>
-      <Button color='blue'>White and Blue</Button>
-      <Button color='red'>White and Red</Button>
-    </div>
+    <main>
+      <h1>Placeholder</h1>
+    </main>
   )
 }
 
